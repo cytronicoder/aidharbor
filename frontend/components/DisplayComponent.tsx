c379e3d97664a9b8b3b0e17a69d0956d3005700a
Zeyu Yao <novodoodle@gmail.com>
2023-09-13 17:20:45 +0800

    Add live count of demo balance

--- frontend/components/DisplayComponent.tsx
+++ frontend/components/DisplayComponent.tsx
@@ -5,6 +5,7 @@ import React, { useState } from "react";
 import { Provider, Network } from "aptos";
 import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
 import { useWallet } from "@aptos-labs/wallet-adapter-react";
+import { useEffect } from "react";
 
 const provider = new Provider(Network.TESTNET);
 const moduleAddress = "0x46237378154b23618ecabe046cf1832f536766eb095813c2b1265845e05d9adb";
@@ -24,10 +25,26 @@ interface DisplayComponentProps {
 }
 
 const DisplayComponent: React.FC<DisplayComponentProps> = ({ jsonToBeDisplayed }) => {
-
     const [, setTransactionInProgress] = useState<boolean>(false);
     const { account, signAndSubmitTransaction } = useWallet();
     const [, setAccountHasList] = useState<boolean>(false);
+    const [totalAptRaised, setTotalAptRaised] = useState(null);
+
+    useEffect(() => {
+        setAccountHasList(true);
+        const apiUrl = 'https://fullnode.testnet.aptoslabs.com/v1/accounts/46237378154b23618ecabe046cf1832f536766eb095813c2b1265845e05d9adb/resource/0x46237378154b23618ecabe046cf1832f536766eb095813c2b1265845e05d9adb::CharityDonation::Charities';
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                // Access the "total_apt_raised" value from the JSON response
+                const totalAptRaisedValue = data.data.total_apt_raised;
+                setTotalAptRaised(totalAptRaisedValue);
+                console.log('totalAptRaisedValue: ', totalAptRaisedValue);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }, [account?.address]);
 
     const donateNow = async () => {
         if (!account) {
@@ -58,7 +75,10 @@ const DisplayComponent: React.FC<DisplayComponentProps> = ({ jsonToBeDisplayed }
 
     return (
         <>
-            <h1 className="title">Charities donation demo ⬇️</h1>
+            <div className="header">
+                <h1 className="title">Charities donation demo ⬇️</h1>
+                <p className="subtitle">Total amount raised in demo account: {totalAptRaised} Octas</p>
+            </div>
             <div className="container">
                 <div className="display-component">
                     <Marquee className='marquee' direction="left" pauseOnHover={true} speed={100}>
